c7dd21c345424a69ed93a215bd75c71492eac973
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-11-16 23:35:00 +0900

    Multiplay の基本部分の機能を追加した

---
+++ Assets/NetworkPlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayerController : MonoBehaviour
+{
+    [SerializeField] Camera m_mainCamera;
+
+    public void Initialize()
+    {
+        m_mainCamera.gameObject.SetActive(true);
+        GetComponent<PlayerInputHandler>().enabled = true;
+        GetComponent<PlayerCharacterController>().enabled = true;
+    }
+}
