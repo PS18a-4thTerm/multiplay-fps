aafe4d92730d288df8a9aea3e282b0bc55dba644
mnwxr335 <mnwxr335@yahoo.co.jp>
2020-01-20 11:35:48 +0900

    コルーチンを使用し、issueを解決

---
+++ Assets/HealthPickupInstantiate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class HealthPickupInstantiate : MonoBehaviour
+{
+    [SerializeField] string m_healthPackPrefab = "";
+    void Start()
+    {
+        StartCoroutine(WaitForConnect());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient)
+        //{
+        //    GameObject healthPack = PhotonNetwork.Instantiate(m_healthPackPrefab, this.transform.position, Quaternion.identity);
+        //}
+    }
+
+    IEnumerator WaitForConnect()
+    {
+        while (!PhotonNetwork.IsConnected || !PhotonNetwork.IsMasterClient)
+        {
+            yield return null;
+        }
+        GameObject healthPack = PhotonNetwork.Instantiate(m_healthPackPrefab, this.transform.position, Quaternion.identity);
+    }
+}
