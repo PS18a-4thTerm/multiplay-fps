7a9f076c2142a862f8b4f90ed5e79a7d9372b23e
mnwxr335 <mnwxr335@yahoo.co.jp>
2020-01-27 10:56:17 +0900

    coroutinで管理

---
+++ Assets/LootHealthPickupInstance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class LootHealthPickupInstance : MonoBehaviour
+{
+    [SerializeField] string m_lootHealthPackPrefab = "";
+    void Start()
+    {
+        StartCoroutine(WaitForConnect());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient)
+        //{
+        //    GameObject healthPack = PhotonNetwork.Instantiate(m_healthPackPrefab, this.transform.position, Quaternion.identity);
+        //}
+    }
+
+    IEnumerator WaitForConnect()
+    {
+        Debug.Log("lootfirstHealDrop");
+        while (!PhotonNetwork.IsConnected)
+        {
+            Debug.Log("lootwaitinghealDrop");
+            yield return null;
+        }
+        GameObject healthPack = PhotonNetwork.Instantiate(m_lootHealthPackPrefab, this.transform.position, Quaternion.identity);
+        Debug.Log("lootSecondhealDrop");
+    }
+}
